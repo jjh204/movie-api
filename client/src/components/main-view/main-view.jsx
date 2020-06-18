174a03963fde15e343d963b355505541d09c4de5
jjh204 <59932565+jjh204@users.noreply.github.com>
2020-06-18 13:28:43 +0200

    updated endpoint api link to main-view

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -1,26 +1,15 @@
 import React from 'react';
 import axios from 'axios';
 
-class MainView extends React.Component {
+export class MainView extends React.Component {
   constructor() {
-    // call the superclass constructor so React can initialize it
     super();
 
-    // initalize state to empty object so we can deconstruct later
     this.state = {};
   }
 
-  // overriding the render() method of superclass (no need to call super() it does nothing by default)
-  render() {
-    return (
-      <div className="main-view"></div>
-    );
-  }
-}
-
-export class MainView extends React.Component {
   componentDidMount() {
-    axios.get('<my-api-endpoint/movies')
+    axios.get('<https://superflix-api.herokuapp.com/movies>')
       .then(response => {
         // assign the result to the state
         this.setState({
