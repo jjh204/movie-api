739bab42a0e5e2220bd156c81b3e60df4e9483a6
jjh204 <59932565+jjh204@users.noreply.github.com>
2020-06-18 12:35:08 +0200

    importing the main-view page into index page

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// importing the main-view from directory
+import { MainView } from './components/main-view/movie-view';
+
 import './index.scss';
 
 // main componetn that will eventually use all the others
 class SuperFlixApplication extends React.Component {
   render() {
-    return (
-      <div className="super-flix">
-        <div>Good Morning!</div>
-      </div>
-    );
+    return <MainView />;
   }
 }
 
@@ -19,3 +18,4 @@ const container = document.getElementsByClassName('app-container')[0];
 
 // tells react to render the app in the root DOM element
 ReactDOM.render(React.createElement(SuperFlixApplication), container);
+
