d34a376cd431578db61757a4f2af7703ae618071
jjh204 <59932565+jjh204@users.noreply.github.com>
2020-06-19 12:18:36 +0200

    updated parcel bundler files

--- client/dist/src.78399e21.js
+++ client/dist/src.78399e21.js
@@ -31503,7 +31503,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50138" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50337" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
