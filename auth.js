d4d9758bdaf92ff343c1f7039961a47358f62152
jjh204 <59932565+jjh204@users.noreply.github.com>
2020-06-22 13:40:26 +0200

    updated endpoint for login page

--- auth.js
+++ auth.js
@@ -3,24 +3,24 @@ const jwtSecret = 'your_jwt_secret'; // same key as created in JWTStrategy
 const jwt = require('jsonwebtoken'),
   passport = require('passport');
 
-  require('./passport'); //my local passport file
+require('./passport'); //my local passport file
 
-  let generateJWTToken = (user) => {
-    return jwt.sign(user, jwtSecret, {
-      subject: user.Username,
-      expiresIn: '7d',
-      algorithm: 'HS256' //this is the algorithm used to 'sign' or encode the values of the JWT
-    });
-  }
+let generateJWTToken = (user) => {
+  return jwt.sign(user, jwtSecret, {
+    subject: user.Username,
+    expiresIn: '7d',
+    algorithm: 'HS256' //this is the algorithm used to 'sign' or encode the values of the JWT
+  });
+}
 
 //POST login.
 module.exports = (router) => {
-  router.post('/users/login', (req, res) => {
+  router.post('/login', (req, res) => {
     passport.authenticate('local', { session: false }, (error, user, info) => {
       if (error || !user) {
         return res.status(400).json({
-            message: 'Something is not right',
-            user: user
+          message: 'Something is not right',
+          user: user
         });
       }
       req.login(user, { session: false }, (error) => {
