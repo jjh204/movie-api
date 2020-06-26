8f8d9ddd1b7daf6bae6aac8ae5ba6609887b2d97
jjh204 <59932565+jjh204@users.noreply.github.com>
2020-06-26 12:51:39 +0200

    updated mongoose connect

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ require('./passport');
 
 const { check, validationResult } = require('express-validator');
 
-/*mongoose.connect('mongodb://localhost:27017/SFlixDatabase', { useNewUrlParser: true, useUnifiedTopology: true }); */
+/* mongoose.connect('mongodb://localhost:27017/SFlixDatabase', { useNewUrlParser: true, useUnifiedTopology: true }); */
 mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(morgan('common'));
