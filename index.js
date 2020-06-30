6cb1f36c8c7fe791a2f47b0175af6a3a63fb4d5e
jjh204 <59932565+jjh204@users.noreply.github.com>
2020-06-30 14:53:16 +0200

    updated cors code

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ let allowedOrigins = ['http://localhost:8080/', 'http://localhost:1234/', 'https
 app.use(cors({
   origin: (origin, callback) => {
     if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) !== -1) {
+    if (allowedOrigins.indexOf(origin) === -1) {
       let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
       return callback(new Error(message), false);
     }
