df5ac266a1f7eedb83ce65e23ca8f9467bd2253b
jjh204 <59932565+jjh204@users.noreply.github.com>
2020-07-03 14:55:28 +0200

    adding a profile-update view

---
+++ client/src/components/profile-update/profile-update.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import PropTypes from 'prop-types';
+
+import { Link } from "react-router-dom";
+
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import './profile-update.scss';
+
+
+export function ProfileUpdate(props) {
+  const [username, updateUsername] = useState('');
+  const [password, updatePassword] = useState('');
+  const [email, updateEmail] = useState('');
+  const [birthday, updateBirthday] = useState('');
+
+  const handleUpdate = (e) => {
+    e.preventDefault();
+
+    const token = localStorage.getItem('token');
+    const userId = localStorage.getItem('user');
+
+    axios.put(`https://superflix-api.herokuapp.com/users/${userId}`,
+      {
+        Username: username,
+        Password: password,
+        Email: email,
+        Birthday: birthday,
+      },
+      { headers: { Authorization: `Bearer ${token}` } }
+    ).then((res) => {
+      const data = res.data;
+      localStorage.setItem("user", data.Username);
+      alert('Your profile was updated successfully.');
+      window.open("/users/:userId", "_self");
+    })
+      .catch((e) => {
+        console.log(e);
+        alert('There was an error updating your profile.');
+      });
+  };
+
+  return (
+    <Container>
+      <Form className="profile-form">
+        <Form.Group controlId="formBasicUsername" className="update-item">
+          <Form.Label>Username:</Form.Label>
+          <Form.Control type="text" value={username} onChange={e => updateUsername(e.target.value)} placeholder="Update username" />
+          <Form.Text className="text-muted">
+            Must have a minimim of 5 characters
+      </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicpassword" className="update-item">
+          <Form.Label>Password:</Form.Label>
+          <Form.Control type="password" value={password} onChange={e => updatePassword(e.target.value)} placeholder="Update password" />
+          <Form.Text className="text-muted">
+            We recommend your password to be 8-20 characters long
+      </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicEmail" className="update-item">
+          <Form.Label>Email:</Form.Label>
+          <Form.Control type="email" value={email} onChange={e => updateEmail(e.target.value)} placeholder="Update email" />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicBirthday" className="update-item">
+          <Form.Label>Birthday:</Form.Label>
+          <Form.Control type="date" value={birthday} onChange={e => updateBirthday(e.target.value)} />
+        </Form.Group>
+
+        <p>We'll never share your private details with anyone else.</p>
+
+        <Button onClick={handleUpdate} variant="primary" type="submit" className="button" >Submit</Button>
+        <Link to={"/users/:userId"}>
+          <Button variant="outline-primary" className="button">Cancel</Button>
+        </Link>
+      </Form>
+    </Container>
+  )
+};
+
+ProfileUpdate.propTypes = {
+  user: PropTypes.shape({
+    Username: PropTypes.string.isRequired,
+    Email: PropTypes.string.isRequired,
+    Password: PropTypes.string.isRequired,
+    Birthday: PropTypes.instanceOf(Date).isRequired
+  })
+};
