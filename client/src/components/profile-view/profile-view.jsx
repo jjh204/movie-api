e2d6a9259899389f06ad6e1a4a55e7f10bb2f566
jjh204 <59932565+jjh204@users.noreply.github.com>
2020-07-07 14:01:05 +0200

    update to delete user warnings

--- client/src/components/profile-view/profile-view.jsx
+++ client/src/components/profile-view/profile-view.jsx
@@ -53,10 +53,9 @@ export class ProfileView extends React.Component {
     });
   }
 
-  deleteUser() {
-    const token = localStorage.getItem('token');
+  deleteUser(token) {
     const userId = localStorage.getItem('user');
-    if (!confirm('Are you sure you want to continue?')) return;
+    if (!confirm('Are you sure you want to delete your profile?')) return;
     axios.delete(`https://superflix-api.herokuapp.com/users/${userId}`, {
       headers: { Authorization: `Bearer ${token}` }
     }).then((res) =>
