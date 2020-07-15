e46bc8763c588468595842c1643495e2fede3aaa
jjh204 <59932565+jjh204@users.noreply.github.com>
2020-07-15 15:22:44 +0200

    setting props.setuser for page refresh

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -40,6 +40,7 @@ export class MainView extends React.Component {
       /* this.setState({
          user: localStorage.getItem('user'),
        }); */
+      this.props.setUser(localStorage.getItem('user'));
       this.getMovies(accessToken);
     }
   }
