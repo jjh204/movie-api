c659fbd60130e4fe98bdd9869039f9bfdfc826c1
jjh204 <59932565+jjh204@users.noreply.github.com>
2020-07-17 14:29:21 +0200

    added navbar to specific views

--- client/src/components/genre-view/genre-view.jsx
+++ client/src/components/genre-view/genre-view.jsx
@@ -3,6 +3,10 @@ import PropTypes from 'prop-types';
 
 import { Link } from "react-router-dom";
 
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import './genre-view.scss';
@@ -15,22 +19,48 @@ export class GenreView extends React.Component {
     this.state = {};
   }
 
+  onLogOut() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    window.open('/', '_self');
+  }
+
   render() {
     const { genre } = this.props;
 
     if (!genre) return null;
 
     return (
-      <Card style={{ width: '50rem' }} className="genre-card">
-        <Card.Img variant="top" src={genre.Img} />
-        <Card.Body>
-          <Card.Title className="genre-name">{genre.Name}</Card.Title>
-          <Card.Text>{genre.Description}</Card.Text>
-          <Link to={"/"}>
-            <Button variant="link" className="button genre-view-back">Back</Button>
-          </Link>
-        </Card.Body>
-      </Card>
+      <div>
+        <Navbar collapseOnSelect expand="lg" bg="custom" variant="dark" className="fixed-top navbar-main">
+          <Navbar.Brand as={Link} to="/" className="navbar-brand">SuperFlix!</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link as={Link} to="/" className="navbar-link">Home</Nav.Link>
+              <Nav.Link as={Link} to="/users/:userId" className="navbar-link">Profile</Nav.Link>
+              <NavDropdown title="About Developer" id="collasible-nav-dropdown" className="navbar-link">
+                <NavDropdown.Item href="https://jjh204.github.io/portfolio-website" target="_blank">Profile</NavDropdown.Item>
+                <NavDropdown.Item href="https://github.com/jjh204" target="_blank">GitHub</NavDropdown.Item>
+                <NavDropdown.Item href="www.linkedin.com/in/jenhobbs204" target="_blank">LinkedIn</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Button onClick={this.onLogOut} variant="dark" type="submit" className="button log-out-button"> Log Out</Button>
+          </Navbar.Collapse>
+        </Navbar>
+        <Container className="genre-view-container">
+          <Card style={{ width: '45rem' }} className="genre-card">
+            <Card.Img variant="top" src={genre.Img} style={{ maxHeight: 400 }} />
+            <Card.Body>
+              <Card.Title className="genre-name">{genre.Name}</Card.Title>
+              <Card.Text>{genre.Description}</Card.Text>
+              <Link to={"/"}>
+                <Button variant="link" className="button genre-view-back">Back</Button>
+              </Link>
+            </Card.Body>
+          </Card>
+        </Container>
+      </div>
     );
   }
 }
