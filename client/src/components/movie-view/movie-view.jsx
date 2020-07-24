87d70430072d86d62c52b59ff3cc661c1c73f7d3
jjh204 <59932565+jjh204@users.noreply.github.com>
2020-07-24 14:42:19 +0200

    removing navbar and update styles

--- client/src/components/movie-view/movie-view.jsx
+++ client/src/components/movie-view/movie-view.jsx
@@ -4,9 +4,6 @@ import PropTypes from 'prop-types';
 
 import { Link } from "react-router-dom";
 
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
@@ -49,22 +46,6 @@ export class MovieView extends React.Component {
 
     return (
       <div>
-        <Navbar collapseOnSelect expand="lg" bg="custom" variant="dark" className="fixed-top navbar-main">
-          <Navbar.Brand as={Link} to="/" className="navbar-brand">SuperFlix!</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link as={Link} to="/" className="navbar-link">Home</Nav.Link>
-              <Nav.Link as={Link} to="/users/:userId" className="navbar-link">Profile</Nav.Link>
-              <NavDropdown title="About Developer" id="collasible-nav-dropdown" className="navbar-link">
-                <NavDropdown.Item href="https://jjh204.github.io/portfolio-website" target="_blank">Profile</NavDropdown.Item>
-                <NavDropdown.Item href="https://github.com/jjh204" target="_blank">GitHub</NavDropdown.Item>
-                <NavDropdown.Item href="https://www.linkedin.com/in/jenhobbs204/" target="_blank">LinkedIn</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Button onClick={this.onLogOut} variant="dark" type="submit" className="button log-out-button"> Log Out</Button>
-          </Navbar.Collapse>
-        </Navbar>
         <Container className="movie-view-container">
           <div className="movie-view">
             <img src={movie.ImagePath} className="movie-view-img" />
@@ -89,6 +70,10 @@ export class MovieView extends React.Component {
             </Card>
           </div>
         </Container>
+        <footer>
+          <p>Designed and created by Jen Hobbs. </p>
+          <p>Movie data from IMDB. Photo's from UnSplash.</p>
+        </footer>
       </div>
     );
   }
